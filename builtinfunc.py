eddb3ac51f0513cbd8b85b78bc6e1a92b3b5404f
Rgmg16 <gumborobert7@gmail.com>
2024-05-20 12:34:38 +0300

    Built in python functions

---
+++ builtinfunc.py
@@ -0,0 +1,12 @@
+#print function
+
+print("Python innit")
+
+#input function
+
+input("Insert name: ")
+
+#len function
+
+length={1,2,3,4,4,5,5,5,6}
+print("The lenght of the object is " , len(length))
