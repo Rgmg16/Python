aae51ff248da1c2a08a72ee48c9aba07c9102f32
Rgmg16 <gumborobert7@gmail.com>
2024-05-22 11:20:19 +0300

    Lists in python

--- lists.py
+++ lists.py
@@ -30,3 +30,64 @@ print (fruits)
 fruits.pop(1)
 
 print (fruits)
+
+#Extending a list
+moreFruits=["avocado", "orange","tomato"]
+fruits.extend(moreFruits)
+
+print(fruits)
+
+#Getting a portion of a list
+fruitsJnr= fruits[0:4:2]
+
+print (fruitsJnr)
+
+#Getting a protion of a list whilst skipping items in the list
+print (fruitsJnr)
+
+#Iterating through a list
+for fruit in fruits:
+    print(fruit)
+    
+#Finding the lenth of a list
+length = len(fruits)
+
+print(length)
+
+#Sorting a list in alphabetical order
+sortFruits= fruits.sort()
+
+print (fruits)
+
+#Reversing the order of the list
+fruits.reverse()
+
+print (fruits)
+
+#Returning the index of the first instance of an element in a list
+indexBanana=fruits.index("banana")
+
+print(indexBanana)
+
+#Checking the existence of an element in a list. It returns a boolean value.
+guavaEx= "Guava" in fruits
+
+print(guavaEx)
+
+#Checking the number of occurrences of an element in a list
+appleOc= fruits.count("apple")
+
+print(appleOc)
+
+#List comprehension: Creation of lists by applying an expression to each item in an existing iterable
+#Example 1: generating squares of numbers
+squares=[x**2 for x in range(5)]
+
+print(squares)
+
+#Example 2: Generating even numbers
+evnNo=[x for x in range (10) if x%2==0]
+
+print(evnNo)
+    
+        
